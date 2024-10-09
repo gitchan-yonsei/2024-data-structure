b1106cc3895c78393883490d57bd92f6586483fb
gitchan <eunkeeeee@gmail.com>
2024-10-09 16:21:47 +0900

    feat: doubly linked list

--- pa1/src/main/DoublyLinkedList.java
+++ pa1/src/main/DoublyLinkedList.java
@@ -10,158 +10,145 @@
  */
 
 public class DoublyLinkedList<T> implements IDoublyLinkedList<T> {
-	private Node<T> head;
-	private Node<T> tail;
-	private int size;
-	// you may declare additional variables here.
+    private Node<T> head;
+    private Node<T> tail;
+    private int size;
+    // you may declare additional variables here.
 
-	public DoublyLinkedList() {
-		// implement your constructor here.
-	}
+    public DoublyLinkedList() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
 
-	@Override
-	public int size() {
-		/*
-		 * Input: none
-		 *
-		 * Output: the number of elements in the linked list
-		 */
-		return -1;
-	}
+    @Override
+    public int size() {
+        return this.size;
+    }
 
-	@Override
-	public boolean isEmpty() {
-		/*
-		 * Input: none
-		 *
-		 * Output: whether or not the linked list is empty
-		 */
-		return true;
-	}
+    @Override
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
 
-	@Override
-	public Node<T> getHead() {
-		/*
-		 * Input: none
-		 *
-		 * Output: the first node of the linked list
-		 * 
-		 * Does:
-		 *  - return the first node of the linked list.
-		 *  - if the linked list is empty, return null.
-		 */
-		return null;
-	}
+    @Override
+    public Node<T> getHead() {
+        if (this.isEmpty()) {
+            return null;
+        }
+        return this.head;
+    }
 
-	@Override
-	public Node<T> getTail() {
-		/*
-		 * Input: none
-		 *
-		 * Output: the last node of the linked list
-		 * 
-		 * Does:
-		 *  - return the last node of the linked list.
-		 *  - if the linked list is empty, return null.
-		 */
-		return null;
-	}
+    @Override
+    public Node<T> getTail() {
+        if (this.isEmpty()) {
+            return null;
+        }
+        return this.tail;
+    }
 
-	@Override
-	public void addFirst(T newElement) {
-		/*
-		 * Input: element to be inserted to the list
-		 *
-		 * Output: none
-		 * 
-		 * Does:
-		 *  - insert the given element at the front of the linked list.
-		 */
-		return;
-	}
+    @Override
+    public void addFirst(T newElement) {
+        final Node<T> currHead = this.head;
+        final Node<T> newHead = new Node<>(newElement);
+        newHead.prev = null;
+        newHead.next = currHead;
+        currHead.prev = newHead;
+        this.head = newHead;
+        this.size++;
+    }
 
-	@Override
-	public void addLast(T newElement) {
-		/*
-		 * Input: element to be inserted to the list
-		 *
-		 * Output: none
-		 * 
-		 * Does:
-		 *  - insert the given element at the back of the linked list.
-		 */
-		return;
-	}
+    @Override
+    public void addLast(T newElement) {
+        final Node<T> currTail = this.tail;
+        final Node<T> newTail = new Node<>(newElement);
+        currTail.next = newTail;
+        newTail.prev = currTail;
+        newTail.next = null;
+        this.tail = newTail;
+        this.size++;
+    }
 
-	@Override
-	public T popFirst() {
-		/*
-		 * Input: none
-		 *
-		 * Output: the first element of the linked list
-		 * 
-		 * Does:
-		 *  - remove and return the first element in the linked list.
-		 *  - if the linked list is empty return null.
-		 */
-		return null;
-	}
+    @Override
+    public T popFirst() {
+        if (this.isEmpty()) {
+            return null;
+        }
 
-	@Override
-	public T popLast() {
-		/*
-		 * Input: none
-		 *
-		 * Output: the last element of the linked list
-		 * 
-		 * Does:
-		 *  - remove and return the last element in the linked list.
-		 *  - if the linked list is empty return null.
-		 */
-		return null;
-	}
+        final Node<T> currHead = this.head;
+        final Node<T> newHead = currHead.next;
+        newHead.prev = null;
+        this.head = newHead;
+        this.size--;
 
-	@Override
-	public Node<T> search(T target) {
-		/*
-		 * Input: target element
-		 *
-		 * Output: the node containing the target element
-		 * 
-		 * Does:
-		 *  - find and return the first node containing an element which is equal to the target element.
-		 *  - check the equivalence by using the method T.equals().
-		 *  - if there is no target element, return null.
-		 */
-		return null;
-	}
+        return currHead.data;
+    }
 
-	@Override
-	public void insertAfter(T newElement, T target) {
-		/*
-		 * Input: element to be inserted to the list, target element
-		 *
-		 * Output: none
-		 * 
-		 * Does:
-		 *  - find the first node containing an element which is equal to the target element.
-		 *  - insert the new element next to the target element.
-		 *  - if there is no target element, do not insert the new element.
-		 */
-		return;
-	}
+    @Override
+    public T popLast() {
+        if (this.isEmpty()) {
+            return null;
+        }
 
-	@Override
-	public void searchDelete(T target) {
-		/*
-		 * Input: target element
-		 *
-		 * Output: none
-		 * 
-		 * Does:
-		 *  - find and remove the first node containing an element which is equal to the target element.
-		 *  - if there is no target element, do nothing.
-		 */
-		return;
-	}
+        final Node<T> currTail = this.tail;
+        final Node<T> newTail = currTail.prev;
+        newTail.next = null;
+        this.tail = newTail;
+        this.size--;
+
+        return currTail.data;
+    }
+
+    @Override
+    public Node<T> search(T target) {
+        Node<T> currNode = this.head;
+        while (true) {
+            if (currNode.data.equals(target)) {
+                return currNode;
+            }
+            currNode = currNode.next;
+            if (currNode == this.tail) {
+                return null;
+            }
+        }
+    }
+
+    @Override
+    public void insertAfter(T newElement, T target) {
+        final Node<T> targetNode = search(target);
+        if (targetNode == null) {
+            return;
+        }
+
+        if (targetNode == this.tail) {
+            addLast(newElement);
+            return;
+        }
+
+        final Node<T> newNode = new Node<>(newElement);
+
+        final Node<T> newNext = targetNode.next;
+        targetNode.next = newNode;
+        newNode.prev = targetNode;
+        newNode.next = newNext;
+        newNext.prev = newNode;
+
+        this.size++;
+    }
+
+    @Override
+    public void searchDelete(T target) {
+        final Node<T> targetNode = search(target);
+        if (targetNode == null) {
+            return;
+        }
+
+        final Node<T> targetPrev = targetNode.prev;
+        final Node<T> targetNext = targetNode.next;
+
+        targetPrev.next = targetNext;
+        targetNext.prev = targetPrev;
+
+        this.size--;
+    }
 }
