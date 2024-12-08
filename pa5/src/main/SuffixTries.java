34b3f22bdcbce6dfecb7b934da90e32afc5d2008
gitchan <eunkeeeee@gmail.com>
2024-12-09 01:17:22 +0900

    feat: suffix tries

--- pa5/src/main/SuffixTries.java
+++ pa5/src/main/SuffixTries.java
@@ -1,6 +1,6 @@
 /*
- * Name:
- * Student ID #:
+ * Name: 조은기
+ * Student ID #: 2019147029
  */
 
 import java.util.ArrayList;
@@ -16,36 +16,83 @@ public class SuffixTries implements ISuffixTries {
     private String s;
     // you may declare additional variables here.
 
-    public SuffixTries(String s){
+    public SuffixTries(String s) {
         this.s = s;
         this.root = new TrieNode();
         /*
          * implement your constructor here. the default constructor is not required.
          * construct the suffix trie from a given string.
          */
+        for (int i = 0; i < s.length(); i++) {
+            addSuffix(i);
+        }
+    }
+
+    private void addSuffix(final int start) {
+        TrieNode current = root;
+
+        for (int i = start; i < s.length(); i++) {
+            final int index = s.charAt(i) - 'a';
+            if (current.children[index] == null) {
+                current.children[index] = new TrieNode();
+            }
+            current = current.children[index];
+        }
+
+        current.isEnd = true;
     }
 
     @Override
-    public boolean match(String pattern){
+    public boolean match(String pattern) {
         /*
-		 * Input: a string pattern
-		 *
-		 * Output: whether or not the given pattern is a suffix of the string
-		 */
-        return false;
+         * Input: a string pattern
+         *
+         * Output: whether or not the given pattern is a suffix of the string
+         */
+        TrieNode current = root;
+
+        for (int i = 0; i < pattern.length(); i++) {
+            final int index = pattern.charAt(i) - 'a';
+            if (current.children[index] == null) {
+                return false;
+            }
+            current = current.children[index];
+        }
+
+        return current.isEnd;
     }
-    
+
     @Override
-    public ArrayList<String> allSuffixes(){
+    public ArrayList<String> allSuffixes() {
         /*
-		 * Input: none
-		 *
-		 * Output: the ArrayList containing all suffixes of the string with lexicographical order
-		 * 
+         * Input: none
+         *
+         * Output: the ArrayList containing all suffixes of the string with lexicographical order
+         *
          * Does:
          *  - traverse suffix tries while maintaining substring
          *  - if the current node is end of a suffix, add the current string to the ArrayList
          */
-        return null;
+        ArrayList<String> result = new ArrayList<>();
+        StringBuilder currentString = new StringBuilder();
+
+        collectSuffixes(root, currentString, result);
+        return result;
+    }
+
+    private void collectSuffixes(TrieNode node, StringBuilder currentString, ArrayList<String> result) {
+        if (node == null) return;
+
+        if (node.isEnd) {
+            result.add(currentString.toString());
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (node.children[i] != null) {
+                currentString.append((char) ('a' + i));
+                collectSuffixes(node.children[i], currentString, result);
+                currentString.deleteCharAt(currentString.length() - 1);
+            }
+        }
     }
 }
